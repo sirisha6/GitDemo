9fec40b776a80f9dfd7f3d4000f50e9f932fe9cb
sirisha6 <sirishatulla@gmail.com>
2022-08-08 20:31:34 +0530

    First Commit

--- MangoTreeA2.java
+++ MangoTreeA2.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MangoTreeA2 {
-}
+import java.util.Scanner;
+public class MangoTreeA2
+    {
+        public static void main(String args[])
+        {
+            Scanner sc=new Scanner(System.in);
+            int rows=sc.nextInt();
+            int columns=sc.nextInt();
+            int treeNumber=sc.nextInt();
+            if(treeNumber%rows==1)
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+    }
