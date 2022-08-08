a62130738a3979a41c93a146f8bb1ca0572f693f
sirisha6 <sirishatulla@gmail.com>
2022-08-08 15:17:10 +0530

    Lab Code

---
+++ Example.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Example {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int n=sc.nextInt();
+        if(x>=n){
+            System.out.println("L1");
+        }
+        else if (y>=n){
+            System.out.println("L2");
+        }
+        else{
+            System.out.println("L3");
+        }
+    }
+}
