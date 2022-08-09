fe61f63209fc28bfd0b19c786ef15228170e2a5d
sirisha6 <sirishatulla@gmail.com>
2022-08-09 13:01:38 +0530

    Median

--- MeanMedianMode.java
+++ MeanMedianMode.java
@@ -1,17 +1,33 @@
 import java.util.*;
 public class MeanMedianMode {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int length=sc.nextInt();
-        int array[]=new int[length];
-        for(int i=0;i<length;i++){
-            array[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int array[] = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = sc.nextInt();
         }
         int sum = 0;
         for (int i = 0; i < length; i++) {
             sum += array[i];
         }
-        System.out.println("Mean of the array  :  "+sum);
-        
+        System.out.println("Mean of the array  :  " + sum);
+
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int temp = 0;
+                if (array[j] < array[i]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        if (length % 2 != 0) {
+            System.out.println("Medain of the array : " + (double) array[length / 2]);
+        } else {
+            System.out.println("Medain of the array : " + (double) (array[(length - 1) / 2] + array[length / 2]) / 2.0);
+        }
     }
 }
