863be56c2f410799a780ac2f2a390ef62fee9347
sirisha6 <sirishatulla@gmail.com>
2022-08-09 11:20:58 +0530

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                int temp = 0;
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+            System.out.println("Mimum number:"+arr[0]);
+            System.out.print("Maximum number:"+arr[4]);
+
+        }
+}
